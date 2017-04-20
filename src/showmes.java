4f82935bc7f8596c6eff9286255ca8c5ca5ed2fe
markfu007 <yufu8027@uni.sydney.edu.au>
2017-04-20 10:26:10 +1000

    Initial commit

---
+++ src/showmes.java
@@ -0,0 +1,12 @@
+/**
+ * Created by edward on 20/04/2017.
+ */
+public class showmes {
+
+        public static void main(String[] args) {
+            System.out.println("Hello World!"); // Display the string.
+        }
+    }
+
+
+
